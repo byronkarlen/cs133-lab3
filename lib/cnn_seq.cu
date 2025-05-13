bfd83fca11414635bfab4775dcf61c9ba0e67167
Byron Karlen <byronkarlen@gmail.com>
2025-05-13 08:09:20 -0700

    added comments

--- lib/cnn_seq.cu
+++ lib/cnn_seq.cu
@@ -4,14 +4,15 @@
 // Sequential CNN implementation
 void cnn_seq(
     const float *input,
-    const float *weight,
-    const float *bias,
+    const float *weight, // kNum (output filters) x kNum (input filters) x kKernel x kKernel
+    const float *bias, // one bias for each filter (256 total)
     float *output
   ) {
 
   // Allocate memory on heap to avoid stack overflow.
-  auto c_size = kNum * kImSize * kImSize * sizeof(float);
+  auto c_size = kNum * kImSize * kImSize * sizeof(float); // size of image after convolution
   float *C = static_cast<float*>(malloc(c_size));
+  // i x h x w
 
   // Bias
   for (int i = 0; i < kNum; ++i) {
@@ -23,13 +24,14 @@ void cnn_seq(
   }
 
   // Convolution
-  for (int i = 0; i < kNum; ++i) {
-    for (int j = 0; j < kNum; ++j) {
-      for (int h = 0; h < kImSize; ++h) {
-        for (int w = 0; w < kImSize; ++w) {
-          for (int p = 0; p < kKernel; ++p) {
-            for (int q = 0; q < kKernel; ++q)
+  for (int i = 0; i < kNum; ++i) { // output channel
+    for (int j = 0; j < kNum; ++j) { // input channel
+      for (int h = 0; h < kImSize; ++h) { // height
+        for (int w = 0; w < kImSize; ++w) { // width
+          for (int p = 0; p < kKernel; ++p) { // kernel height
+            for (int q = 0; q < kKernel; ++q) { // kernel width
               C(i,h,w) += weight(i,j,p,q) * input(j,h+p,w+q);
+            }
           }
         }
       }
@@ -46,7 +48,7 @@ void cnn_seq(
   }
 
   // Max pooling
-  for (int i = 0; i < kNum; ++i) {
+  for (int i = 0; i < kNum; ++i) { // i refers to output filter channel
     for (int h = 0; h < kOutImSize; ++h) {
       for (int w = 0; w < kOutImSize; ++w) {
         output(i,h,w) = max(
